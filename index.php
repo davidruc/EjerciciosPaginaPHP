e5c6e661f55834b598ecc3248b02d638d3a85a62
davidruc <davidarueda18@gmail.com>
2023-07-01 20:55:19 -0500

    feat: :rocket: Decimo ejercicio

--- index.php
+++ index.php
@@ -1,25 +1,31 @@
-<?php
+<?php 
 
-class Bob
+/*
+ * By adding type hints and enabling strict type checking, code can become
+ * easier to read, self-documenting and reduce the number of potential bugs.
+ * By default, type declarations are non-strict, which means they will attempt
+ * to change the original type to match the type specified by the
+ * type-declaration.
+ *
+ * In other words, if you pass a string to a function requiring a float,
+ * it will attempt to convert the string value to a float.
+ *
+ * To enable strict mode, a single declare directive must be placed at the top
+ * of the file.
+ * This means that the strictness of typing is configured on a per-file basis.
+ * This directive not only affects the type declarations of parameters, but also
+ * a function's return type.
+ *
+ * For more info review the Concept on strict type checking in the PHP track
+ * <link>.
+ *
+ * To disable strict typing, comment out the directive below.
+ */
+
+declare (strict_types = 1);
+function toRna(string $dna): string
 {
-    public function respondTo(string $phrase): string
-    {
-        $phrase = trim($phrase, "\n\r \t\u{000b}\u{00a0}\u{2002}}");
-        // Silence
-        if (empty($phrase)) {
-            return 'Fine. Be that way!';
-        }
-        // Shouting
-        if (preg_match('/[A-Z]+/', $phrase) && !preg_match('/[a-z]+/', $phrase)) {
-            if (str_ends_with($phrase, '?')) {
-                return 'Calm down, I know what I\'m doing!';
-            }
-            return 'Whoa, chill out!';
-        }
-        // Question
-        if (str_ends_with($phrase, '?')) {
-            return 'Sure.';
-        }
-        return 'Whatever.';
-    }
+    return strtr($dna, ['G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U']);
 }
+
+?>
